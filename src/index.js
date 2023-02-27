ecbe0fa202d4f36275c4a98089b4625df3eb4773
Ivan <ivanvolodin960@gmail.com>
2023-02-27 14:39:18 +0300

    feat: add class with method for sending requests to api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
+import 'regenerator-runtime/runtime';
+import axios from "axios";
+
 import "./index.html";
 import "./index.scss";
 
-import { mult, sum } from "./modules/calc";
+const url = "https://gateway.marvel.com/v1/public/comics";
+class GetDataApi {
+  async getData(urlApi) {
+    try {
+      const response = await axios.get(urlApi, {
+        params: {
+          apikey: process.env.API_KEY,
+          limit: 50
+        }
+      });
+      return response.data.data.results;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
 
-console.log(mult(3, 4));
-console.log(sum(3, 4));
-console.log(process.env.SECRET_KEY);
+const getDataApi = new GetDataApi();
+
+(async () => {
+  const data = await getDataApi.getData(url);
+
+  if (data) {
+    console.log(data);
+  } else {
+    console.log("Error: " + data);
+  }
+})();
