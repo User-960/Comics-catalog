ed8f8c7df64fc7a2d3152e64a261fe370a362963
Ivan <ivanvolodin960@gmail.com>
2023-02-27 15:13:33 +0300

    refactor: move the class for sending requests to the server to a separate module

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://gateway.marvel.com/v1/public/";
 export const URL_COMICS = "comics";
-// export const URL_CHARACTERS = "characters";
+export const URL_CHARACTERS = "characters";
--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import 'regenerator-runtime/runtime';
-import axios from "axios";
 import { API_URL, URL_COMICS } from "./constants/api";
+import { getDataApi } from "./utils/getDataApi";
 
 import "./index.html";
 import "./index.scss";
-class GetDataApi {
-  async getData(url) {
-    try {
-      const response = await axios.get(url, {
-        params: {
-          apikey: process.env.API_KEY,
-          limit: 50
-        }
-      });
-      return response.data.data.results;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  }
-}
-
-const getDataApi = new GetDataApi();
 
 (async () => {
   const data = await getDataApi.getData(API_URL + URL_COMICS);
