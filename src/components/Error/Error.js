681970987854f5aed7f0c8e3a004b905fbbe9ef4
Ivan <ivanvolodin960@gmail.com>
2023-03-01 02:04:34 +0300

    feat: add styles for class error instance

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,12 +1,13 @@
+import classes from "./Error.scss";
 import { ROOT_INDEX } from "../../constants/root";
 import ErrorIcon from "../../images/error.svg";
 
 class Error {
   render() {
     const html = `
-      <div class="error__inner">
-        <img class="error__img" src="${ErrorIcon}" alt="Error! No access."/>
-        <div class="error__message">
+      <div class="${classes.error__inner}">
+        <img class="${classes.error__img}" src="${ErrorIcon}" alt="Error! No access."/>
+        <div class="${classes.error__message}">
           <h3>No access!</h3>
           <p>Try again later.</p>
         </div>
