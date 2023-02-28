3afc89de97aad560858a0e418b0abdf8f9374c04
Ivan <ivanvolodin960@gmail.com>
2023-02-28 14:45:22 +0300

    feat: creat method in class comics for processing data coming from api

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -1,6 +1,7 @@
-import { API_URL, URL_COMICS } from "../../constants/api";
+import {
+ API_URL, URL_COMICS, IMG_STANDART_XLARGE, IMG_NOT_AVAILABLE
+} from "../../constants/api";
 import { getDataApi } from "../../utils/getDataApi";
-
 import { ROOT_INDEX } from "../../constants/root";
 
 class Comics {
@@ -8,12 +9,35 @@ class Comics {
     const data = await getDataApi.getData(API_URL + URL_COMICS);
 
     if (data) {
-      console.log(data);
+      let htmlContent = "";
+      /**
+     * @param [Number] id - identification of item
+     * @param [String] title - title of item
+     * @param [Object] thumbnail => [String] extension - jpg/png/svg
+     * @param [Object] thumbnail => [String] path - http://...
+     */
+      data.forEach(({ id, title, thumbnail: { path, extension } }) => {
+        console.log(id, title);
+
+        if (path.lastIndexOf(IMG_NOT_AVAILABLE) === -1) {
+          const imgSrc = path + "/" + IMG_STANDART_XLARGE + "." + extension;
+            htmlContent += `
+              <li>
+                <span>${title}</span>
+                <img src="${imgSrc}" alt="comic book cover"/>
+              </li>
+            `;
+          }
+      });
+      const htmlWrapper = `
+        <ul>
+          ${htmlContent}
+        </ul>
+        `;
+      ROOT_INDEX.innerHTML = htmlWrapper;
     } else {
       console.log("Error: " + data);
     }
-
-    ROOT_INDEX.innerHTML = "<h1>Hello Webpack</h1>";
   }
 }
 
